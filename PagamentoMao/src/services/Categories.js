3b2dced9d52914917923cad208c4b2500c9c25fe
wendelcliques <70993835+wendelcliques@users.noreply.github.com>
2020-11-04 03:55:55 -0300

    atual

---
+++ PagamentoMao/src/services/Categories.js
@@ -0,0 +1,36 @@
+import firestore from '@react-native-firebase/firestore';
+
+export const getDefaultCategories = () => {
+    return [
+        {
+            name: 'Alimentação',
+            color: '#1acb9c',
+            isDebit: true,
+            order: 0,
+        },
+
+        {
+            name: 'Empréstimos',
+            color: '#273c75',
+            isCredit: true,
+            order: 1,
+        },
+
+        {
+            name: 'Saldo Inicial',
+            color: '#27ae60',
+            isInit: true,
+            order: 5,
+        },
+
+    ];
+}]
+
+export const getInitCategories = async () => {
+    const querySnapshot = await firestore()
+        .collection('categories')
+        .where('isInit', '==', true)
+        .get();
+
+    return {...querySnapshot.docs[0].data(), id: querySnapshot.docs[0].id};
+};
