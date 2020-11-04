9ef38e05dc110cdbba934b9dc4f5878c1c904984
wendelcliques <70993835+wendelcliques@users.noreply.github.com>
2020-11-04 01:33:01 -0300

    atual

---
+++ PagamentoMao/src/components/ListaProdutos.js
@@ -0,0 +1,37 @@
+//import React, {useEffect, useState} from 'react';
+import { View, Text, FlatList } from 'react-native'
+
+
+import styles from '../styles/index';
+
+const ListaProdutos = () => {
+    /*[entries, setEntries] = useState([]);
+
+    useEffect(() => {
+      async function loadEntries() {
+        const data = await getEntries();
+        setEntries(data);
+      }
+  
+      loadEntries();
+  
+      console.log('EntryList :: useEffect');
+    }, []);*/
+
+
+    return (
+        <View>
+            <Text>Lista de Produtos</Text>
+            <FlatList
+        data={entries}
+        renderItem={({item}) => (
+          <Text style={styles.entry}>
+            - {item.description} - ${item.amount}
+          </Text>
+        )}
+      />
+        </View>
+    )
+}
+
+export default ListaProdutos
